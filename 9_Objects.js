215927902549782cc12fa7f477f639e259413ab2
Mouqueer Anwar <roncodes13@gmail.com>
2024-08-26 19:14:39 +0530

    myplaground added

--- 9_Objects.js
+++ 9_Objects.js
@@ -1,52 +1,54 @@
 // OBJECTS --------------------------------------------------------------------------------
-  /* Objects are type of variable, quite similar to array but they have something called key-value pairs */
+/* Objects are type of variable, quite similar to array but they have something called key-value pairs */
 
-  // const person = {
-  //   name: 'Emma Watson' ,
-  //   shirt: 'white'
-  // }
-  
-  //  ACCESS OBJECT - 
-  //  Dot notation
-  // console.log(person.name);
-  // console.log(person.shirt);
+// const person = {
+//   name: 'Emma Watson' ,
+//   shirt: 'white'
+// }
 
-  // Bracet Notation
-  // console.log(person['name']);
-  // console.log(person['shirt']);
+//  ACCESS OBJECT -
+//  Dot notation
+// console.log(person.name);
+// console.log(person.shirt);
 
-  // Assign object
-  // person['phone'] = '+910000000000'
-  // console.log(person.phone)
+// Bracet Notation
+// console.log(person['name']);
+// console.log(person['shirt']);
 
-  // console.log(person)
+// Assign object
+// person['phone'] = '+910000000000'
+// console.log(person.phone)
 
-  // 
+// console.log(person)
 
-  // const person2 = {
-  //   name: 'Mouqueer' ,
-  //   shirt: ' black'
-  // }
-  // console.log
+//
 
-  // ES6 arrow functions (2 arguments)
-  // Objects
-  // Template literals
-  // Methods - a method is a property containing a function defination
+// const person2 = {
+//   name: 'Mouqueer' ,
+//   shirt: ' black'
+// }
+// console.log
 
-  const introducer = (name , shirt ) => {
-    const person = {
-      name: name ,
-      shirt: shirt,
-      assets: 100000,
-      liabilities: 50000,
-      netWorth: function() {
-        return this.assets - this.liabilities
-      }
-    }
+// ES6 arrow functions (2 arguments)
+// Objects
+// Template literals
+// Methods - a method is a property containing a function defination
 
-    const intro = `Hey my name is ${person.name} and my shirt colour is ${person.shirt} and my net worth is $${person.netWorth()} US dollars`
-    return(intro)
-  }
+const introducer = (name, shirt) => {
+  const person = {
+    name: name,
+    shirt: shirt,
+    assets: 100000,
+    liabilities: 50000,
+    netWorth: function () {
+      return this.assets - this.liabilities;
+    },
+  };
 
-  console.log(introducer('mouuqeer' , 'black'));
+  const intro = `Hey my name is ${person.name} and my shirt colour is ${
+    person.shirt
+  } and my net worth is $${person.netWorth()} US dollars`;
+  return intro;
+};
+
+console.log(introducer("mouuqeer", "black"));
