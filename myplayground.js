29e289e2c7c47aa0e63bfc3abcb2e79d0e3cc383
roncodes <roncodes13@gmail.com>
2024-08-29 03:48:27 +0530

    checking

--- myplayground.js
+++ myplayground.js
@@ -1,1 +1,29 @@
-console.log("Hello world");
+// // Objects
+
+// const introducer = (name, shirt) => {
+//   const person = {
+//     name: name,
+//     shirt: shirt,
+//     assets: 10000,
+//     liabialities: 5000,
+//     netWorth: function () {
+//       return this.assets - this.liabialities;
+//     },
+//   };
+//   const intro = `Hello my name is ${person.name} , i am wearing ${
+//     person.shirt
+//   } and my net worth is ${person.netWorth()}`;
+//   return intro;
+// };
+// console.log(introducer("moqueer", "black"));
+
+// Loops
+
+const fruits = ["🍎", "🍌", "🍇", "🍊", "🍍"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+const fruit = ["🍇", "🍊", "🍍"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
